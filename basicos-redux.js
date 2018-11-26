902bfb150349be08a1d4caf5e5a5cd78ae5586ff
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-11-26 12:46:19 -0600

    se agrego subscribe

--- basicos-redux.js
+++ basicos-redux.js
@@ -2,26 +2,29 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 //state inicial 
-const stateInicial ={
+const stateInicial = {
     usuarios : []
 }
 
 //Reducer
 
-const reducerPrincipal = (state = stateInicial, action) =>{
+const reducerPrincipal = (state = stateInicial, action) => {
 
-    if(action.type == 'ADD_USER'){
+    /*
+    if(action.type === 'ADD_USER'){
         return {
             ...state,
             usuarios : action.name
         }
     }
+    */
+    /*
     if(action.type == 'GET_USER'){
         return {
             ...state
         }
 
-    }
+    }*/
 
     return state;
 }
@@ -30,13 +33,13 @@ const reducerPrincipal = (state = stateInicial, action) =>{
 // el store es que guardare el state de la aplicacion 
 //utiliza tre sparametros 2 son opcionales  reducer, state Inicial  applyMiddleware
 // Reducer es necesario es el encargado de como se cambia el state
-const store = createStore(reducerPrincipal); 
+//const store = createStore(reducerPrincipal); 
 
-console.log(store.getState());
+//console.log(store.getState());
 
-store.dispatch({ type: 'ADD_USER', name: 'Ricardo'});
-store.dispatch({ type: 'GET_USER'});
+//store.dispatch({ type: 'ADD_USER', name: 'Ricardo'});
+//store.dispatch({ type: 'GET_USER'});
 
 
 
-console.log(store.getState());
+//console.log(store.getState());
